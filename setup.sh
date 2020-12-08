3b9040ccec22aa5cd540c9c8de5327d3455ece73
liubomyr.ivanitskyi <liubomyr.ivanitskyi@globallogic.com>
2020-12-08 13:27:04 +0200

    Fix parent dir

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-rm -r weights
-mkdir weights
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo DIR
-wget -nc -O "${DIR}"/weights/pose_hrnet_w32_256x192.pth https://www.dropbox.com/s/85toprbvlzzslyk/pose_hrnet_w32_256x192.pth?dl=0
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+rm -f -r "$SCRIPTPATH/weights" > /dev/null
+mkdir "$SCRIPTPATH/weights"
+wget -nc -O "${SCRIPTPATH}/weights/pose_hrnet_w32_256x192.pth" https://www.dropbox.com/s/85toprbvlzzslyk/pose_hrnet_w32_256x192.pth?dl=0
