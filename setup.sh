00eeb53e30812bcd409321042d4d41a7a095df64
liubomyr.ivanitskyi <liubomyr.ivanitskyi@globallogic.com>
2020-12-08 13:05:30 +0200

    Fix parent dir

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rm -r weights
 mkdir weights
-wget -nc -O weights/pose_hrnet_w32_256x192.pth https://www.dropbox.com/s/85toprbvlzzslyk/pose_hrnet_w32_256x192.pth?dl=0
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo DIR
+wget -nc -O "${DIR}"/weights/pose_hrnet_w32_256x192.pth https://www.dropbox.com/s/85toprbvlzzslyk/pose_hrnet_w32_256x192.pth?dl=0
